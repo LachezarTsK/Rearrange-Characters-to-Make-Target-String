3c13f9c4e1f6f19002dc6cbf2085a0b62d40870a
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-25 11:11:24 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,14 +3,14 @@ public class Solution {
 
     private static final int ALPHABET_SIZE = 26;
 
-    public int rearrangeCharacters(String availablelLetters, String target) {
-        int[] frequencyInAvailablelLetters = new int[ALPHABET_SIZE];
-        calculateFrequency(frequencyInAvailablelLetters, availablelLetters);
+    public int rearrangeCharacters(String availableLetters, String target) {
+        int[] frequencyInAvailableLetters = new int[ALPHABET_SIZE];
+        calculateFrequency(frequencyInAvailableLetters, availableLetters);
 
         int[] frequencyInTarget = new int[ALPHABET_SIZE];
         calculateFrequency(frequencyInTarget, target);
 
-        return calculateMaxNumberOfTargetCopies(frequencyInAvailablelLetters, frequencyInTarget);
+        return calculateMaxNumberOfTargetCopies(frequencyInAvailableLetters, frequencyInTarget);
     }
 
     private void calculateFrequency(int[] frequency, String source) {
@@ -19,12 +19,12 @@ public class Solution {
         }
     }
 
-    private int calculateMaxNumberOfTargetCopies(int[] frequencyInAvailablelLetters, int[] frequencyInTarget) {
+    private int calculateMaxNumberOfTargetCopies(int[] frequencyInAvailableLetters, int[] frequencyInTarget) {
         int maxNumberOfTargetCopies = Integer.MAX_VALUE;
 
         for (int letter = 0; letter < ALPHABET_SIZE; ++letter) {
             if (frequencyInTarget[letter] > 0) {
-                int numberOfCopies = frequencyInAvailablelLetters[letter] / frequencyInTarget[letter];
+                int numberOfCopies = frequencyInAvailableLetters[letter] / frequencyInTarget[letter];
                 maxNumberOfTargetCopies = Math.min(numberOfCopies, maxNumberOfTargetCopies);
             }
         }
